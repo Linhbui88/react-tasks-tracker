fd4801409886daa6f598fa216acbe26f8eceda0f
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2021-12-26 17:29:50 -0500

    handle complete/remove task

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,14 +5,28 @@ const Task = ({task,tasks, setTasks}) => {
   const handleRemoveTask = () =>{
     setTasks(tasks.filter(el=> el.id!== task.id))
   }
+  const handleCompleteTask = () =>{
+    setTasks(tasks.map(el =>{
+      if(el.id === task.id) {
+        return{
+          ...el, isCompleted: !task.isCompleted
+        }
+      }
+      return el;
+    }))
+}
+
+
   return (
    
     <div className="bg-white text-black m-1 w-25 text-break d-flex flex-row align-items-start justify-content-between">
-      <div className='mx-2'>
+      <div className='mx-2' style={{
+        textDecoration: task.isCompleted? 'line-through' : '',
+        opacity: task.isCompleted?'0.5' : ''}}>
       {task.text}
       </div>
       <div className='d-flex flex-row'>
-        <button><FaCheckSquare /></button>
+        <button onClick={handleCompleteTask}><FaCheckSquare /></button>
         <button onClick={handleRemoveTask}><FaTrashAlt /></button>
       </div>
     </div>
